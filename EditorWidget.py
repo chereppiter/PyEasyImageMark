8dfe76615541624e5900af93f5bfd50bef2fd97a
pavlovsky <pavlovsky@protei.ru>
2021-02-14 11:38:02 +0300

    Refactoring for undo support

--- EditorWidget.py
+++ EditorWidget.py
@@ -1,16 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QScrollArea, QMenu, QApplication
-from PyQt5.QtWidgets import QAction, QShortcut, QActionGroup
-from PyQt5.QtGui import QKeySequence
-from PyQt5.QtGui import QMouseEvent, QWheelEvent, QContextMenuEvent
-from PyQt5.QtGui import QPainter, QPen, QColor, QImage
-from PyQt5.QtCore import QEvent, QPoint, QPointF, QRectF, QSizeF, Qt
+from PyQt5.QtWidgets import (QWidget, QScrollArea, QMenu, QApplication,
+                             QAction, QShortcut, QActionGroup)
+from PyQt5.QtGui import QKeySequence, QMouseEvent, QWheelEvent, QContextMenuEvent
+from PyQt5.QtCore import QEvent, Qt
 from ImageWidget import ImageWidget
-from enum import IntEnum
-
-
-class _LeftButtonMode(IntEnum):
-    Paint = 1
-    Move = 2
 
 
 class EditorWidget(QScrollArea):
@@ -24,32 +16,37 @@ class EditorWidget(QScrollArea):
         self._horz_scroll_bar = self.horizontalScrollBar()
         self._vert_scroll_bar = self.verticalScrollBar()
         self._clipboard = QApplication.clipboard()
-        self._source_image = None
-        self._image = None
         self._last_move_pos = None
-        self._set_scale_factor(1.0)
+        self._lines = []
+
+        self._init_actions()
+
+    def _init_actions(self):
 
         self._context_menu = QMenu(self)
 
-        self._context_menu.addSection("Left Button Mode")
+        self._context_menu.addSection("Mouse Mode")
         mode_action_group = QActionGroup(self)
-        self._paint_mode_action = self._context_menu.addAction("Paint")
-        self._paint_mode_action.setCheckable(True)
-        self._paint_mode_action.setChecked(True)
-        mode_action_group.addAction(self._paint_mode_action)
         self._move_mode_action = self._context_menu.addAction("Move")
         self._move_mode_action.setCheckable(True)
+        self._move_mode_action.setChecked(True)
         mode_action_group.addAction(self._move_mode_action)
+        self._paint_mode_action = self._context_menu.addAction("Paint")
+        self._paint_mode_action.setCheckable(True)
+        mode_action_group.addAction(self._paint_mode_action)
+        mode_action_group.triggered.connect(self._on_mouse_mode_action_checked)
+        self._on_mouse_mode_action_checked(mode_action_group.checkedAction())
 
         self._context_menu.addSection("Pen Width")
-        self._pen_width_action_group = QActionGroup(self)
+        pen_width_action_group = QActionGroup(self)
         for width in (2, 3, 5, 7, 10, 14, 20):
             action = self._context_menu.addAction(str(width) + " px")
             action.setData(width)
             action.setCheckable(True)
-            self._pen_width_action_group.addAction(action)
+            pen_width_action_group.addAction(action)
             if width == 5:
                 action.setChecked(True)
+        pen_width_action_group.triggered.connect(self._on_pen_width_action_checked)
 
         self._context_menu.addSeparator()
         copy_action = QAction("Copy", self)
@@ -63,49 +60,40 @@ class EditorWidget(QScrollArea):
         reset_scale_action = self._context_menu.addAction("Actual size")
         reset_scale_action.triggered.connect(self._reset_scale)
         clear_action = self._context_menu.addAction("Clear")
-        clear_action.triggered.connect(self._clear)
+        clear_action.triggered.connect(self._image_widget.clear)
 
         paste_shortcut = QShortcut(paste_action.shortcut(), self)
         paste_shortcut.activated.connect(paste_action.trigger)
         copy_shortcut = QShortcut(copy_action.shortcut(), self)
-        copy_shortcut.activated.connect(clear_action.trigger)
+        copy_shortcut.activated.connect(copy_action.trigger)
 
-    def _set_scale_factor(self, factor: float) -> None:
-        self._scale_factor = factor
-        self._image_widget.set_scale_factor(factor)
-        if self._has_image():
-            self._image_widget.resize(self._image.size() * self._scale_factor)
+    def _on_mouse_mode_action_checked(self, action: QAction):
+        self._image_widget.set_paint_enabled(action == self._paint_mode_action)
 
-    def _paste(self):
+    def _on_pen_width_action_checked(self, action: QAction) -> None:
+        selected_pen_width = int(action.data())
+        self._image_widget.set_pen_width(selected_pen_width)
+
+    def _paste(self) -> None:
 
         print("paste")
         mime_data = self._clipboard.mimeData()
         if mime_data.hasImage():
             image = self._clipboard.image()
             if not image.isNull():
-                self._set_scale_factor(1.0)
-                self._source_image = image
-                self._restore_image()
+                self._image_widget.set_image(image)
             else:
                 print("Image is null")
         else:
             print("Clipboard has no image")
 
-    def _copy(self):
-        if self._has_image():
-            self._clipboard.setImage(self._image)
+    def _copy(self) -> None:
+        print("copy")
+        if self._image_widget.has_image():
+            self._clipboard.setImage(self._image_widget.get_complex_image())
 
     def _reset_scale(self):
-        self._set_scale_factor(1.0)
-
-    def _clear(self):
-        if (self._source_image is not None) and (not self._source_image.isNull()):
-            self._restore_image()
-
-    def _restore_image(self):
-        self._image = QImage(self._source_image)
-        self._image_widget.set_image(self._image)
-        self._image_widget.resize(self._image.size())
+        self._image_widget.set_scale_factor(1.0)
 
     def viewportEvent(self, event: QEvent) -> bool:
 
@@ -126,71 +114,42 @@ class EditorWidget(QScrollArea):
     def _is_moving(self) -> bool:
         return self._last_move_pos is not None
 
-    def _left_button_mode(self) -> _LeftButtonMode:
-        if self._paint_mode_action.isChecked():
-            return _LeftButtonMode.Paint
-        return _LeftButtonMode.Move
-
     def _handle_mouse_press_event(self, event: QMouseEvent) -> None:
-
-        if self._has_image() and (event.button() == Qt.LeftButton) and (event.buttons() == Qt.LeftButton):
+        if (self._image_widget.has_image() and
+                (event.button() == Qt.LeftButton) and (event.buttons() == Qt.LeftButton)):
             self._last_move_pos = event.pos()
         elif self._is_moving():
             self._last_move_pos = None
 
     def _handle_mouse_release_event(self, event: QMouseEvent) -> None:
-        if (event.button() == Qt.LeftButton) and self._is_moving():
+        if self._is_moving():
             self._last_move_pos = None
 
-    def _get_pen_width(self) -> int:
-        pen_width_action = self._pen_width_action_group.checkedAction()
-        if pen_width_action is None:
-            return 0
-        return round(int(pen_width_action.data()) * self._scale_factor)
-
     def _handle_mouse_move_event(self, event: QMouseEvent) -> None:
-
-        if not self._has_image():
+        if not self._image_widget.has_image():
             return
-
         if not self._is_moving():
             return
-
-        if self._left_button_mode() == _LeftButtonMode.Paint:
-            painter = QPainter(self._image)
-            viewport_top_left = QPointF(self._image_widget.mapFromParent(QPoint(0, 0))) / self._scale_factor
-            viewport_size = QSizeF(self.viewport().size()) / self._scale_factor
-            viewport_rect = QRectF(viewport_top_left, viewport_size)
-            painter.setViewport(viewport_rect.toAlignedRect())
-            painter.setWindow(self.viewport().rect())
-            pen = QPen(QColor(255, 0, 0))
-            pen.setWidth(self._get_pen_width())
-            painter.setPen(pen)
-            painter.drawLine(self._last_move_pos, event.pos())
-            self._image_widget.update()
-        else:
-            offset = event.pos() - self._last_move_pos
-            self._horz_scroll_bar.setValue(self._horz_scroll_bar.value() - offset.x())
-            self._vert_scroll_bar.setValue(self._vert_scroll_bar.value() - offset.y())
+        offset = event.pos() - self._last_move_pos
+        self._horz_scroll_bar.setValue(self._horz_scroll_bar.value() - offset.x())
+        self._vert_scroll_bar.setValue(self._vert_scroll_bar.value() - offset.y())
         self._last_move_pos = event.pos()
 
     def _handle_wheel_event(self, event: QWheelEvent) -> None:
 
-        if not self._has_image():
-            super().wheelEvent(event)
+        if not self._image_widget.has_image():
             return
 
         angle_delta_y = event.angleDelta().y()
 
         if angle_delta_y == 0:
-            super().wheelEvent(event)
             return
 
         scale_factor_factor = 1.1
         if angle_delta_y < 0:
             scale_factor_factor = 1.0 / scale_factor_factor
         old_center = self._image_widget.mapFromParent(self.viewport().rect().center())
-        self._set_scale_factor(self._scale_factor * scale_factor_factor)
+        self._image_widget.set_scale_factor(self._image_widget.get_scale_factor() * scale_factor_factor)
 
         new_center = old_center * scale_factor_factor
         offset = new_center - old_center
@@ -199,6 +158,3 @@ class EditorWidget(QScrollArea):
 
     def _handle_context_menu_event(self, event: QContextMenuEvent) -> None:
         self._context_menu.exec_(event.globalPos())
-
-    def _has_image(self) -> bool:
-        return (self._image is not None) and (not self._image.isNull())
--- ImageWidget.py
+++ ImageWidget.py
@@ -1,32 +1,148 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QPaintEvent, QPainter, QImage, QWheelEvent
-from PyQt5.QtCore import QSize, QSizeF, QRectF, QPointF
+from PyQt5.QtGui import QPaintEvent, QPainter, QPen, QColor, QImage, QMouseEvent
+from PyQt5.QtCore import QSizeF, QRectF, QPoint, QPointF, Qt
+
+
+class _Line:
+
+    def __init__(self, p1: QPointF, p2: QPointF) -> None:
+        self._p1 = p1
+        self._p2 = p2
+
+    def draw(self, painter: QPainter, scale_factor: float = 1.0) -> None:
+        painter.drawLine(self._p1 * scale_factor, self._p2 * scale_factor)
+
+
+class _LineSet:
+
+    def __init__(self, pen_width: float) -> None:
+        self._pen_width = pen_width
+        self._lines = []
+
+    def add_line(self, line: _Line) -> None:
+        self._lines.append(line)
+
+    def draw(self, painter: QPainter, scale_factor: float = 1.0) -> None:
+        pen = QPen(QColor(255, 0, 0))
+        pen.setWidthF(self._pen_width * scale_factor)
+        painter.setPen(pen)
+        for line in self._lines:
+            line.draw(painter, scale_factor)
 
 
 class ImageWidget(QWidget):
 
     def __init__(self, parent: QWidget = None):
         super().__init__(parent)
-
         self._image = None
+        self._reset()
+        self._pen_width = 5
+        self._paint_enabled = True
+
+    def _reset(self) -> None:
         self._scale_factor = 1.0
+        self._lines = []
+        self._current_line_set = None
+        self._last_pen_pos = None
 
     def set_image(self, image: QImage) -> None:
         self._image = image
+        self._reset()
+        self.resize(image.size())
         self.update()
 
     def set_scale_factor(self, factor: float) -> None:
         self._scale_factor = factor
+        if self.has_image():
+            self.resize(self._image.size() * self._scale_factor)
+            self.update()
+
+    def get_scale_factor(self) -> float:
+        return self._scale_factor
+
+    def set_pen_width(self, width: int) -> None:
+        self._pen_width = width
+
+    def set_paint_enabled(self, enabled: bool) -> None:
+        self._paint_enabled = enabled
+        if not enabled and self._last_pen_pos is not None:
+            self._finish_paint()
+
+    def has_image(self) -> bool:
+        return (self._image is not None) and (not self._image.isNull())
+
+    def get_complex_image(self) -> QImage:
+
+        if self._image is None:
+            return QImage()
+
+        image = QImage(self._image)
+        painter = QPainter(image)
+        for line_set in self._lines:
+            line_set.draw(painter)
+        return image
+
+    def clear(self) -> None:
+        print("clear")
+        self._lines = []
         self.update()
+
+    def _get_target_pen_width(self) -> float:
+        return self._pen_width / self._scale_factor
 
     def paintEvent(self, event: QPaintEvent) -> None:
 
         if self._image is None:
             super().paintEvent(event)
+
         else:
             painter = QPainter(self)
+
+            # Draw image
             target_size = QSizeF(self._image.size()) * self._scale_factor
             target_rect = QRectF(QPointF(0, 0), target_size)
             source_rect = QRectF(self._image.rect())
             painter.drawImage(target_rect, self._image, source_rect)
 
+            # Draw lines
+            for line_set in self._lines:
+                line_set.draw(painter, self._scale_factor)
+
+    def _add_line(self, pos: QPoint) -> None:
+        line = _Line(self._last_pen_pos / self._scale_factor, pos / self._scale_factor)
+        self._current_line_set.add_line(line)
+
+    def _finish_paint(self, pos: QPoint) -> None:
+        if self._last_pen_pos is not None:
+            self._add_line(pos)
+            self._last_pen_pos = None
+            self._current_lines = None
+            self.update()
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if not self._paint_enabled:
+            super().mousePressEvent(event)
+            return
+        if (event.button() == Qt.LeftButton) and (event.buttons() == Qt.LeftButton):
+            self._last_pen_pos = event.pos()
+            self._current_line_set = _LineSet(self._get_target_pen_width())
+            self._lines.append(self._current_line_set)
+        else:
+            self._finish_paint(event.pos())
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if not self._paint_enabled:
+            super().mouseMoveEvent(event)
+            return
+        if self._last_pen_pos is not None:
+            pos = event.pos()
+            self._add_line(pos)
+            self._last_pen_pos = pos
+            self.update()
+
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        if not self._paint_enabled:
+            super().mouseReleaseEvent(event)
+            return
+        if self._last_pen_pos is not None:
+            self._finish_paint(event.pos())
